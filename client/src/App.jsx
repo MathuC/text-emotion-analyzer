42da31ba52fde2e9384718fabf2daa42dc235fe9
MathuC <mathu99430@hotmail.com>
2025-09-05 18:09:42 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          <center>
+              <h1>Welcome to my simple app!</h1>
+              <p>This is a basic example using Vite and React.</p>
+          </center>
+      </div>
+  );
+}
+
+export default App
---
+++ server/app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='../client/dist')
+CORS(app)
+
+# since npm run build puts the react build into assets folder
+@app.route('/assets/<path:filename>')
+def serve_static(filename):
+    return app.send_static_file(f'assets/{filename}')
+
+@app.route('/')
+def serve():
+    return app.send_static_file('index.html')
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify({"message": "Hello from Flask!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=3000)
