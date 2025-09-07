ec400c8fc007318825610c43d014aaaaed55612a
MathuC <mathu99430@hotmail.com>
2025-09-07 00:47:43 -0400

    style changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
+import "./App.css"
 
 function App() {
-  return (
-      <div>
-          <center>
-              <h1>Text Emotion Analyzer</h1>
-              <input id="text-input"></input>
-          </center>
-      </div>
-  );
+  	const [text, setText] = useState("");
+  	const [output, setOutput] = useState("");
+
+  	return (
+		<div>
+			<div id="links-container">
+			<div id="gh-logo-container">
+				<a href="https://github.com/MathuC/text-emotion-analyzer">
+				<img id="gh-logo" src='img/ghLogo.png'></img>
+				</a>
+			</div>
+			<div id="my-link-container">
+				<a id="my-link" href='https://mathusan.net'>
+				<span>mathusan.net</span>
+				</a>
+			</div>
+			</div>
+				<h1 id="title">TEXT EMOTION ANALYZER</h1>
+				<center>
+					<textarea id="text-input" placeholder="Type something..."></textarea>
+				</center>
+		</div>
+  	);
 }
 
 export default App
